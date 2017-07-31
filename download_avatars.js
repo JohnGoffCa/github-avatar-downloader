3f5123c2b65523386a71d0e47277aca52527b101
John Goff <gygn.johngoff@gmail.com>
2017-07-31 11:44:40 -0400

    gets the body of the request and pipes it to stdout

--- download_avatars.js
+++ download_avatars.js
@@ -8,7 +8,19 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
   const requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   
-  console.log(requestURL);
+  const options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': "GitHub Avatar Scraper"
+    },
+  }
+
+  request.get(options)
+        .on("error", (err) => {
+          throw err;
+        })
+        .pipe(process.stdout);
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {

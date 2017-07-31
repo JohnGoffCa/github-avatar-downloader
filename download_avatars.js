e2a66a6658801b8a6afcd9bdb2fe33adf733457a
John Goff <gygn.johngoff@gmail.com>
2017-07-31 11:14:09 -0400

    github user tokens added & programmatically generate request URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,14 @@
 var request = require('request');
 
+const GITHUB_USER = "JohnTheScout";
+const GITHUB_TOKEN = "b33459b0c6dd1ca9f0d84ba316e348f9a9837d96";
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  const requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {

419f12885c802bfcc30bdf6c5ae0961d1eb66f3f
John Goff <gygn.johngoff@gmail.com>
2017-07-31 17:07:32 -0400

    recommends the top 5 repos based on target repo

--- recommend.js
+++ recommend.js
@@ -1,15 +1,75 @@
-const getRepo = require("./download_avatars.js")
+const getRepo = require("./download_avatars")
+const request = require("request")
+require('dotenv').config()
 
 if (process.argv.length < 4) {
   console.log("Please provide at least two arguments, organization and repo");
   return;
 }
 
-function countStarred(url, object) {
-  console.log(url)
+let recommendedRepos = {};
+let requestsPending = 0;
+let requestsFinished = 0;
+
+function getStarredByUser(url, object, cb) {
+  const urlWithHeader = {
+    url: url,
+    headers: {
+      'User-Agent': "Count Starred Repos",
+    },
+  }
+
+  let starredByUser = ""
+  request.get(urlWithHeader)
+        .on('error', (err) => {
+          cb(err);
+        })
+        .on('data', (data) => {
+          starredByUser += data;
+        })
+        .on('end', () => {
+          var starredBy = JSON.parse(starredByUser);
+          starredBy.forEach((elem) => {
+            if (object.hasOwnProperty(elem.full_name)) {
+              object[elem.full_name] += 1;
+            } else {
+              object[elem.full_name] = 1
+            }
+          });
+
+          requestsFinished++;
+          if (requestsFinished === requestsPending) 
+            cb(null, object);
+        });
 }
 
-var recommendedRepos = {};
+function printStarred(err, object) {
+  if (err) {
+    throw err
+  }
+
+  let sorted = sortProperties(object);
+  for (let i = 0; i < sorted.length && i < 5; i++) {
+    console.log(`[${sorted[i][1]} stars] - ${sorted[i][0]}`);
+  }
+}
+
+//from https://gist.github.com/umidjons/9614157
+function sortProperties(obj) {
+  // convert object into array
+	var sortable = [];
+	for (var key in obj)
+		if (obj.hasOwnProperty(key))
+			sortable.push([key, obj[key]]); // each item is an array in format [key, value]
+	
+	// sort items by value
+	sortable.sort(function(a, b)
+	{
+	  return b[1]-a[1]; // compare numbers
+	});
+	return sortable; // array in format [ [ key1, val1 ], [ key2, val2 ], ... ]
+}
+
 getRepo(process.argv[2], process.argv[3], (err, result) => {
   if (err)
     console.log("Errors:", err);
@@ -22,7 +82,9 @@ getRepo(process.argv[2], process.argv[3], (err, result) => {
     return;
   }
 
-  for (let i = 0; i < result.length; i++) {
-    countStarred(result[i].starred_url.slice(0, -15), recommendedRepos);
+  console.log(`Based on ${process.argv[2]}/${process.argv[3]} you might like: `);
+  requestsPending = result.length;
+  for (let i = 0; i < requestsPending; i++) {
+    getStarredByUser(`https://${process.env.GITHUB_USER}:${process.env.GITHUB_TOKEN}@api.github.com/users/${result[i].login}/starred`, recommendedRepos, printStarred);
   }
 });
